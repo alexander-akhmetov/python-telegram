9a11e4d8551445083f15a389aeca051979970f58
Alexander Akhmetov <me@alex.earth>
2018-05-01 18:52:51 +0200

    Removed sphinx_readable_theme from docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_readable_theme
 
 
 # -- Project information -----------------------------------------------------

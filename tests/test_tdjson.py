1f4548ff169185444fcaeaf3c5396fd780e9a9c6
Alexander Akhmetov <me@aleks.sh>
2019-11-02 10:25:23 +0000

    Fix tests and remove pytest-mock plugin

--- tests/test_tdjson.py
+++ tests/test_tdjson.py
@@ -1,13 +1,15 @@
+from unittest.mock import Mock, patch
+
 from telegram.tdjson import _get_tdjson_lib_path
 
 
-class Test_get_tdjson_lib_path(object):
-    def test_for_darwin(self, mocker):
-        mocked_system = mocker.Mock(return_value='Darwin')
-        mocked_resource = mocker.Mock()
+class TestGetTdjsonTdlibPath:
+    def test_for_darwin(self):
+        mocked_system = Mock(return_value='Darwin')
+        mocked_resource = Mock()
 
-        with mocker.mock_module.patch('telegram.tdjson.platform.system', mocked_system):
-            with mocker.mock_module.patch(
+        with patch('telegram.tdjson.platform.system', mocked_system):
+            with patch(
                 'telegram.tdjson.pkg_resources.resource_filename', mocked_resource
             ):
                 _get_tdjson_lib_path()
@@ -16,36 +18,36 @@ class Test_get_tdjson_lib_path(object):
             'telegram', 'lib/darwin/libtdjson.dylib'
         )
 
-    def test_for_linux(self, mocker):
-        mocked_system = mocker.Mock(return_value='Linux')
-        mocked_resource = mocker.Mock(return_value='/tmp/')
+    def test_for_linux(self):
+        mocked_system = Mock(return_value='Linux')
+        mocked_resource = Mock(return_value='/tmp/')
 
-        with mocker.mock_module.patch('telegram.tdjson.platform.system', mocked_system):
-            with mocker.mock_module.patch(
+        with patch('telegram.tdjson.platform.system', mocked_system):
+            with patch(
                 'telegram.tdjson.pkg_resources.resource_filename', mocked_resource
             ):
                 _get_tdjson_lib_path()
 
         mocked_resource.assert_called_once_with('telegram', 'lib/linux/libtdjson.so')
 
-    def test_for_windows(self, mocker):
-        mocked_system = mocker.Mock(return_value='Windows')
-        mocked_resource = mocker.Mock(return_value='/tmp/')
+    def test_for_windows(self):
+        mocked_system = Mock(return_value='Windows')
+        mocked_resource = Mock(return_value='/tmp/')
 
-        with mocker.mock_module.patch('telegram.tdjson.platform.system', mocked_system):
-            with mocker.mock_module.patch(
+        with patch('telegram.tdjson.platform.system', mocked_system):
+            with patch(
                 'telegram.tdjson.pkg_resources.resource_filename', mocked_resource
             ):
                 _get_tdjson_lib_path()
 
         mocked_resource.assert_called_once_with('telegram', 'lib/linux/libtdjson.so')
 
-    def test_unknown(self, mocker):
-        mocked_system = mocker.Mock(return_value='Unknown')
-        mocked_resource = mocker.Mock(return_value='/tmp/')
+    def test_unknown(self):
+        mocked_system = Mock(return_value='Unknown')
+        mocked_resource = Mock(return_value='/tmp/')
 
-        with mocker.mock_module.patch('telegram.tdjson.platform.system', mocked_system):
-            with mocker.mock_module.patch(
+        with patch('telegram.tdjson.platform.system', mocked_system):
+            with patch(
                 'telegram.tdjson.pkg_resources.resource_filename', mocked_resource
             ):
                 _get_tdjson_lib_path()

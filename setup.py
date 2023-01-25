3b9d4d3a3bc2a3e69f75325a9d94debe12f51500
Alexander Akhmetov <me@alx.cx>
2023-01-25 22:27:44 +0100

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import re
-from distutils.core import setup
+from setuptools import setup
 
 
 def get_version(package):
@@ -9,6 +9,7 @@ def get_version(package):
     Returns version of a package (`__version__` in `init.py`).
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
+
     return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
@@ -22,6 +23,7 @@ setup(
     author='Alexander Akhmetov',
     author_email='me@aleks.sh',
     url='https://github.com/alexander-akhmetov/python-telegram',
+    long_description_content_type='text/markdown',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     packages=[
         'telegram',

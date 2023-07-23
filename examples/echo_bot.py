f6037804bfc7e7649eb16f7a3fc4113286c3f817
Alexander Akhmetov <me@alx.cx>
2023-07-23 18:33:48 +0200

    Fix #391 check is_outgoing in the echo bot

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -33,8 +33,9 @@ if __name__ == '__main__':
     def new_message_handler(update):
         message_content = update['message']['content']
         message_text = message_content.get('text', {}).get('text', '').lower()
+        is_outgoing = update['message']['is_outgoing']
 
-        if message_content['@type'] == 'messageText' and message_text == 'ping':
+        if not is_outgoing and message_content['@type'] == 'messageText' and message_text == 'ping':
             chat_id = update['message']['chat_id']
             print(f'Ping has been received from {chat_id}')
             tg.send_message(

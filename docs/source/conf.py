65ad366f625ae9d3e6cf5d2e55213072c925cdba
Alexander Akhmetov <me@alx.cx>
2024-06-23 18:49:26 +0200

    ruff format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -165,9 +165,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, "python-telegram", "python-telegram Documentation", [author], 1)
-]
+man_pages = [(master_doc, "python-telegram", "python-telegram Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output ----------------------------------------------
